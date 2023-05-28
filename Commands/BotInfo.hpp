4dc3e02a9747bc3d92af4fc89b6c650cb4a53193
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2023-05-28 06:35:00 -0400

    First commit.

---
+++ Commands/BotInfo.hpp
@@ -0,0 +1,81 @@
+// bot_info.hpp - Header for the "bot info" command.
+// Jun 19, 2021
+// Chris M.
+// https://github.com/RealTimeChris
+
+#pragma once
+
+#include <discordcoreapi/Index.hpp>
+
+namespace discord_core_api {
+
+	class bot_info : public base_function {
+	  public:
+		bot_info() {
+			commandName = "botinfo";
+			helpDescription = "Displays some info about this bot.";
+			embed_data msgEmbed{};
+			msgEmbed.setDescription("------\nEnter /botinfo.\n------");
+			msgEmbed.setTitle("__**Bot Info Usage:**__");
+			msgEmbed.setTimeStamp(getTimeAndDate());
+			msgEmbed.setColor("FeFeFe");
+			helpEmbed = msgEmbed;
+		}
+
+		unique_ptr<base_function> create() {
+			return makeUnique<bot_info>();
+		}
+
+		void execute(const base_function_arguments& argsNew) {
+			try {
+				auto guilds = guilds::getAllGuildsAsync();
+				int32_t userCount{ 0 };
+				for (auto& value: guilds) {
+					userCount += value.memberCount;
+				}
+
+				embed_data messageEmbed;
+				messageEmbed.setAuthor(argsNew.getUserData().userName, argsNew.getUserData().getUserImageUrl(user_image_types::Avatar));
+				messageEmbed.setImage(discord_core_client::getInstance()->getBotUser().getUserImageUrl(user_image_types::Avatar));
+				messageEmbed.setColor("FEFEFE");
+				messageEmbed.setTitle("__**Bot Info:**__");
+				messageEmbed.setTimeStamp(getTimeAndDate());
+				messageEmbed.addField("__Bot Name:__",
+					jsonifier::string{ discord_core_client::getInstance()->getBotUser().userName } + "#" +
+						jsonifier::string{ discord_core_client::getInstance()->getBotUser().discriminator },
+					true);
+				messageEmbed.addField("__Bot ID:__",
+					jsonifier::toString(discord_core_client::getInstance()->getBotUser().id.operator const uint64_t&()), true);
+				messageEmbed.addField("__Guild Count:__", jsonifier::toString(guilds.size()), true);
+				messageEmbed.addField("__Created At:__", discord_core_client::getInstance()->getBotUser().id.getCreatedAtTimeStamp(), true);
+				messageEmbed.addField("__Serving Users:__", jsonifier::toString(userCount), true);
+				messageEmbed.addField("__Running On:__", "[discord_core_api Bot Library](https://discordcoreapi.com)", true);
+				messageEmbed.addField("__Created By:__", "<@931311002702737418>", true);
+				auto timePassed = discord_core_client::getInstance()->getTotalUpTime().count() > 0
+					? discord_core_client::getInstance()->getTotalUpTime().count()
+					: 0;
+				int64_t millisecondsPerSecond{ 1000 };
+				int64_t millisecondsPerMinute{ millisecondsPerSecond * 60 };
+				int64_t millisecondsPerHour{ millisecondsPerMinute * 60 };
+				int64_t millisecondsPerDay{ millisecondsPerHour * 24 };
+				int64_t daysPassed = ( int64_t )trunc(timePassed / millisecondsPerDay);
+				int64_t hoursPassed = ( int64_t )trunc((timePassed % millisecondsPerDay) / millisecondsPerHour);
+				int64_t minutesPassed = ( int64_t )trunc(((timePassed % millisecondsPerDay) % millisecondsPerHour) / millisecondsPerMinute);
+
+				int64_t secondsPassed =
+					( int64_t )trunc((((timePassed % millisecondsPerDay) % millisecondsPerHour) % millisecondsPerMinute) / millisecondsPerSecond);
+				jsonifier::string string{ jsonifier::toString(daysPassed) + " Days, " + jsonifier::toString(hoursPassed) + " Hours, " +
+					jsonifier::toString(minutesPassed) + " Minutes, " + jsonifier::toString(secondsPassed) + " Seconds." };
+				messageEmbed.addField("__Total Uptime:__", string, true);
+				respond_to_input_event_data dataPackage(argsNew.getInputEventData());
+				dataPackage.setResponseType(input_event_response_type::Interaction_Response);
+				dataPackage.addMessageEmbed(messageEmbed);
+				auto eventNew = input_events::respondToInputEventAsync(dataPackage).get();
+				return;
+			} catch (const dca_exception& error) {
+				std::cout << "bot_info::execute()" << error.what() << std::endl;
+			}
+		}
+		~bot_info(){};
+	};
+}
---
+++ main.cpp
@@ -0,0 +1,51 @@
+﻿// main.cpp - Main entry point.
+//// Jun 17, 2021
+// Chris M.
+// https://github.com/RealTimeChris
+
+#include "Commands/BotInfo.hpp"
+
+int32_t main() {
+	std::string botToken = "";
+	discord_core_api::sharding_options shardOptions{};
+	shardOptions.numberOfShardsForThisProcess = 1;
+	shardOptions.startingShard = 0;
+	shardOptions.totalNumberOfShards = 1;
+	discord_core_api::logging_options logOptions{};
+	logOptions.logGeneralErrorMessages = true;
+	logOptions.logHttpsErrorMessages = true;
+	logOptions.logWebSocketErrorMessages = true;
+	logOptions.logHttpsSuccessMessages = true;
+	logOptions.logGeneralSuccessMessages = true;
+	logOptions.logWebSocketSuccessMessages = true;
+	discord_core_api::discord_core_client_config clientConfig{};
+	//clientConfig.connectionAddress = "127.0.0.1";
+	clientConfig.botToken = botToken;
+	clientConfig.logOptions = logOptions;
+	clientConfig.shardOptions = shardOptions;
+	clientConfig.cacheOptions.cacheChannels = true;
+	clientConfig.cacheOptions.cacheGuilds = true;
+	clientConfig.cacheOptions.cacheUsers = true;
+	clientConfig.cacheOptions.cacheRoles = true;
+	jsonifier::vector<discord_core_api::activity_data> activities{};
+	discord_core_api::activity_data activity{};
+	activity.name = "/help for my commands!";
+	activity.type = discord_core_api::activity_type::game;
+	activities.emplace_back(activity);
+	clientConfig.presenceData.activities = activities;
+	clientConfig.presenceData.afk = false;
+	clientConfig.presenceData.status = discord_core_api::presence_update_state::online;
+	clientConfig.textFormat = discord_core_api::text_format::etf;
+	clientConfig.presenceData.since = 0;
+	auto the = makeUnique<discord_core_api::discord_core_client>(clientConfig);
+	discord_core_api::create_global_application_command_data createBotInfoCommandData{};
+	createBotInfoCommandData.dmPermission = true;
+	createBotInfoCommandData.type = discord_core_api::application_command_type::Chat_Input;
+	createBotInfoCommandData.defaultMemberPermissions = discord_core_api::permission::Use_Application_Commands;
+	createBotInfoCommandData.description = "Displays info about the current bot.";
+	createBotInfoCommandData.name = "botinfo";
+	the->registerFunction(jsonifier::vector<jsonifier::string>{ "botinfo" }, discord_core_api::makeUnique<discord_core_api::bot_info>(),
+		createBotInfoCommandData);
+	the->runBot();
+	return 0;
+};
